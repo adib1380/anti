4a37aff16ff8c06be82ac100a7d918d1b2fae183
adib1380 <adib.1380.nj@gmail.com>
2015-12-06 10:33:51 -0800

    Update groupmanager.lua

--- plugins/groupmanager.lua
+++ plugins/groupmanager.lua
@@ -5,7 +5,7 @@ do
 local function create_group(msg)
     -- superuser and admins only (because sudo are always has privilege)
     if not is_admin(msg) then
-        return "You're not admin!"
+        return "You're not admin! Do you want create your antispam group? send a message @adibnikjooo"
     end
     local group_creator = msg.from.print_name
     create_group_chat (group_creator, group_name, ok_cb, false)
